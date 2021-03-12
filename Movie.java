51efd5e6327be70066ed64e73c5f6f6438b75e33
¨RafaelMassoni¨ <rafaelmgb@dcc.ufmg.br>
2021-03-12 16:59:32 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -37,23 +37,7 @@ public class Movie {
     }
 
     public double getCharge(int daysRented){
-        double returnAmount = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                returnAmount += 2;
-                if (daysRented > 2)
-                    returnAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                returnAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                returnAmount += 1.5;
-                if (daysRented > 3)
-                    returnAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return returnAmount;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented){
