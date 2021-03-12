83af5a3a4c7c22d82a7a0b2d92ea2470214926ea
¨RafaelMassoni¨ <rafaelmgb@dcc.ufmg.br>
2021-03-12 17:01:10 -0300

    Commit 13

--- Price.java
+++ Price.java
@@ -1,23 +1,4 @@
 public abstract class Price {
     public abstract int getPriceCode();
-    
-    public double getCharge(int daysRented){
-        double returnAmount = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                returnAmount += 2;
-                if (daysRented > 2)
-                    returnAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                returnAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                returnAmount += 1.5;
-                if (daysRented > 3)
-                    returnAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return returnAmount;
-    }
+    public abstract double getCharge(int daysRented);
 }
